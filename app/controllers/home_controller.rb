cbac215913503ca3d0445bbddd7ed3cbc9d7026d
Dang Quoc Huy <huysamdua@yahoo.com>
2016-10-15 15:51:43 +0700

    finish first 3 required items

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  
   def index
   end
+
+  def contact_us
+	end
+
+	def menu
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
+	get 'contact_us' => 'home#contact_us'
+	get 'menu' => 'home#menu'
 	root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
